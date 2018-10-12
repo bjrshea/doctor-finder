3f7358ec3e3ec4118240e02c04cf239a03de1630
Brendan Shea <padma@email.com>
2018-10-12 14:21:43 -0700

    Add functionality that allow users to search by doctor name.

--- src/main.js
+++ src/main.js
@@ -6,24 +6,27 @@ import BetterDoctor from './BetterDoctor.js';
 
 $(document).ready(function() {
   $('#user-button').click(function() {
-    const symptoms = $('#symptoms').val();
     let newBetterDoctor = new BetterDoctor;
+    const symptoms = $('#symptoms').val();
     $('#symptoms').val('');
+    const doctorFirstName = $('#doctor-first-name').val();
+    $('#doctor-first-name').val('');
+    const doctorLastName = $('#doctor-last-name').val();
+    $('#doctor-last-name').val('');
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${doctorFirstName}&last_name=${doctorLastName}&query=${symptoms}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        newBetterDoctor.firstName = response.data[0].profile.first_name
-        newBetterDoctor.lastName = lastName;
-        newBetterDoctor.street = street;
-        newBetterDoctor.city = city;
-        newBetterDoctor.state = state;
-        newBetterDoctor.zip = zip;
-        newBetterDoctor.phoneNumber = phoneNumber;
-        console.log(newBetterDoctor.firstName);
+        newBetterDoctor.name = response.data[0].practices[0].name
+        newBetterDoctor.street = response.data[0].practices[0].visit_address.street
+        newBetterDoctor.city = response.data[0].practices[0].visit_address.city
+        newBetterDoctor.state = response.data[0].practices[0].visit_address.state
+        newBetterDoctor.zip = response.data[0].practices[0].visit_address.zip
+        newBetterDoctor.phoneNumber = response.data[0].practices[0].phones[0].number
+        console.log(newBetterDoctor);
       },
       error: function() {
         console.log("hi");
